c73f0d2f99ab3525c3eac0be3eb06ee1c6620edf
AnilGulacti <gulactianil02@gmail.com>
2026-01-21 12:54:20 +0300

    Add files via upload

---
+++ sellers/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, re_path
+from products.views import (
+    ProductCreateView,
+    SellerProductListView,
+    ProductUpdateView,
+)
+
+from .views import (
+    SellerDashboard,
+    SellerTransactionListView,
+    SellerProductDetailRedirectView,
+    )
+
+app_name = 'sellers'
+
+urlpatterns = [
+    path('', SellerDashboard.as_view(), name='dashboard'),
+    path('transactions/', SellerTransactionListView.as_view(), name='transactions'),
+    path('products/', SellerProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>/', SellerProductDetailRedirectView.as_view()),
+    path('products/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('products/add/', ProductCreateView.as_view(), name='product_create'),
+]
